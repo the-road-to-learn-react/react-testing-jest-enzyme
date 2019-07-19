2b4a6e8c085f1107c08b476235fb470922d0010e
Robin Wieruch <wrobin@gmx.net>
2019-07-19 18:25:26 +0200

    move test folders

--- src/App.spec.js
+++ src/App.spec.js
@@ -10,38 +10,38 @@ import App, { dataReducer, Counter } from './App';
 
 const list = ['a', 'b', 'c'];
 
-describe('Reducer Test', () => {
-  it('should set a list', () => {
-    const state = { list: [], error: null };
-    const newState = dataReducer(state, {
-      type: 'SET_LIST',
-      list,
-    });
-
-    expect(newState).toEqual({ list, error: null });
-  });
-
-  it('should reset the error if list is set', () => {
-    const state = { list: [], error: true };
-    const newState = dataReducer(state, {
-      type: 'SET_LIST',
-      list,
-    });
-
-    expect(newState).toEqual({ list, error: null });
-  });
-
-  it('should set the error', () => {
-    const state = { list: [], error: null };
-    const newState = dataReducer(state, {
-      type: 'SET_ERROR',
-    });
-
-    expect(newState.error).toBeTruthy();
-  });
-});
-
 describe('App', () => {
+  describe('Reducer', () => {
+    it('should set a list', () => {
+      const state = { list: [], error: null };
+      const newState = dataReducer(state, {
+        type: 'SET_LIST',
+        list,
+      });
+
+      expect(newState).toEqual({ list, error: null });
+    });
+
+    it('should reset the error if list is set', () => {
+      const state = { list: [], error: true };
+      const newState = dataReducer(state, {
+        type: 'SET_LIST',
+        list,
+      });
+
+      expect(newState).toEqual({ list, error: null });
+    });
+
+    it('should set the error', () => {
+      const state = { list: [], error: null };
+      const newState = dataReducer(state, {
+        type: 'SET_ERROR',
+      });
+
+      expect(newState.error).toBeTruthy();
+    });
+  });
+
   test('snapshot renders', () => {
     const component = renderer.create(<App />);
     let tree = component.toJSON();
